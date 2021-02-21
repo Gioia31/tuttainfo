2642aeb76bff8e18ce0ad921f5c06a488f21ebdf
Gioia31 <61776559+Gioia31@users.noreply.github.com>
2021-02-21 19:59:20 +0100

    modifico il file esAtleta.py

--- esAtleta.py
+++ esAtleta.py
@@ -1,32 +1,33 @@
-class Atleta:
-    def __init__ (self, name, age, surname, weight, height):
+class Atleta():
+
+    def __init__(self, name, surname, age, weight, height):
         self.name = name
-        self.age = age
         self.surname = surname
+        self.age = age
         self.weight = weight
         self.height = height
         self.visitaMedica = False
 
+    def info(self):
+        print("L'atleta, ", self.name, self.surname, " di età ", self.age, ", pesa ", self.weight,"kg, ed è alto ", self.height, " cm")
+        if self.visitaMedica == False:
+            print("L'atleta ", self.name, self.surname, " non si è sottoposto alla visita medica ")
+        else:
+            print("L'atleta ", self.name, self.surname, " si è sottoposto alla visita medica")
+
     def effettua_visita(self):
         self.visitaMedica = True
-
-    def info(self):
-        print("L'atleta si chiama: ", self.name, self.surname)
-        print("Età: ", self.age)
-        print("Peso: ", self.weight, " kg")
-        print("Altezza: ", self.height, " cm")
         
-    
-    def squadra( self, team):
+    def squadra(self, team):
         self.team = team
-        print("L'atleta ", self.name, self.surname, " gioca nella squadra ", self.team)
+        print("L'atleta gioca nella squadra ", self.team)
 
 def main():
     name = input("Inserisci il nome dell'atleta ")
-    age = input("Inserisci l'età dell'atleta ")
     surname = input("Inserisci il cognome dell'atleta ")
-    weight = int(input("Inserisci il peso dell'atleta "))
-    height = int(input("Inserisci l'altezza dell'atleta "))
+    age = input("Inserisci l'età dell'atleta ")
+    weight = float(input("Inserisci il peso dell'atleta "))
+    height = float(input("Inserisci l'altezza dell'atleta "))
     team = input("Inserisci la squadra in cui gioca l'atleta ")
     atleta = Atleta(name, surname, age, weight, height)
     visita = str(input("L'atleta si è sottoposto alla visita medica? "))
@@ -36,11 +37,3 @@ def main():
     atleta.squadra(team)
 
 main()
-
-
-
-
-
-    
-
-        
