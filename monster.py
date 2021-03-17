ed67e8c2f630f0da0360533f1342a672f950ecb8
Gioia31 <61776559+Gioia31@users.noreply.github.com>
2021-03-17 22:33:50 +0100

    aggiungo il file monster.py

--- monster.py
+++ monster.py
@@ -1,27 +1,86 @@
-class Monster:
-    def __init__ (self, nome, demage):
-        self.nome = nome
-        self.hp = 10
-        self.demage = demage
-    
-    def info(self):
-        print("sono ", self.nome)
-    
-    def attack (self, enemy):
-        print(self.nome, " attacca ", enemy.nome)
+class Entity: 
+  def __init__(self, x, y, field):
+    self.x = x
+    self.y = y
+    self.field = field
+    self.field.entities.append(self) 
 
-        if (enemy.hp <= 0):
-            print(enemy.nome, " è morto")
+  def move(self, direction):
+    if direction == "up":
+      self.y -= 1
+    elif direction == "down":
+      self.y += 1
+    elif direction == "left":
+      self.x -= 1
+    elif direction == "right":
+      self.x += 1
+      
+
+class Monster(Entity):
+  def __init__(self, x, y, name, damage, field):
+    super().__init__(x, y, field)
+    self.name = name
+    self.hp = 10 #valore prestabilito
+    self.damage = damage
+
+  def info(self):
+    print("Sono il mostro ", self.name)
+    print("Hp: ", self.hp, "/10")
+    print("Mi trovo alle coordinate ", self.x, self.y)
+
+  def attack(self, enemy):
+    if self.hp <= 0:
+      print(self.name, "prova ad attaccare da morto con scarsi risultati")
+    else: 
+      print(self.name, "attacca", enemy.name)
+
+      if (enemy.hp <= 0):
+        print(enemy.name, "e' morto")
+      else:
+        enemy.hp -= self.damage
+
+
+class Field:
+  def __init__(self, w, h):
+    self.w = w
+    self.h = h
+    self.entities = [] 
+
+  def draw(self):
+    print()
+    for y in range(self.h):
+      for x in range(self.w):
+        for e in self.entities:
+          if e.x >= self.w:
+            e.x = self.w -1
+          if e.y >= self.h:
+            e.y = self.h -1
+          if e.x <= 0:
+            e.x = 0
+          if e.y <= 0:
+            e.y = 0
+          if x == e.x and y == e.y:
+            print("[x]", end = "")
+            break    
         else:
-            enemy.hp -= self.demage
+          print("[ ]", end = "" )
+      print()
 
 
-m1 = Monster("Pino", 6)
+field = Field(4,4)
+m = Monster(2, 3, "Pino", 10, field)
+m1 = Monster(0, 0, "Gino", 10, field)
+m.info()
 m1.info()
-
-m2 = Monster("Pluto", 2)
-m2.info()
-
-m1.attack(m2)
-m1.attack(m2)
-m1.attack(m2)
+field.draw()
+m1.attack(m)
+m1.move("down")
+field.draw()
+m1.move("down")
+field.draw()
+m1.move("down")
+field.draw()
+m1.move("right")
+field.draw()
+m1.move("right")
+field.draw()
